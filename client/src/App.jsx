3ed55b68fc464e363efe2d7785a7c1c5361c414b
charafeddine-Web <charafeddinetbibzat@gmail.com>
2024-05-05 22:51:40 +0100

    add Login/register/reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,12 @@
+import Login from "./Authenticate/Login";
+import Register from "./Authenticate/register";
+import ResetPassword from "./Authenticate/ResetPassword";
 import './App.css'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Login from './Auth/Login'
-import Register from './Auth/Register'
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/Register" element={<Register/>} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <Login/>
+  );
 }
 
-export default App
+export default App;
